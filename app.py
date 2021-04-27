842c29f3dc59e62f9309eb86a74fdd73bd2abc49
Franz Tseng <blume918@gmail.com>
2021-04-27 10:18:49 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,51 @@
+import database
+
+menu = """What would you like to do?
+        1) Enter a new note
+        2) Display all note
+        3) Search notes by category
+        4) Search note by labels
+        5) Exit
+Your choice: """
+
+database.create_table()
+user_input = input(menu)
+
+def prompt_new_note():
+    cat = input("Note category: ")
+    lab = input("Note label: ")
+    content = input("Enter your note: ")
+    database.note(cat, lab, content)
+
+def show_note():
+    notes = database.display()
+    for note in notes:
+        print(f"""{note[0]} | {note[1]} 
+-------------------------------------------  
+{note[2]}
+-------------------------------------------\n\n""")
+
+def show_search_result():
+    cat = input("Enter the category your are looking for: ")
+    search_result = database.search_cat(cat) 
+    for search in search_result:
+        print(f"""{search[0]} | {search[1]} 
+-------------------------------------------  
+{search[2]}
+-------------------------------------------\n\n""")
+
+    
+
+
+while user_input !="5":
+    if user_input =="1":
+        prompt_new_note()
+    elif user_input =="2":
+        show_note()
+    elif user_input == "3":
+        show_search_result()
+    elif user_input == "4":
+        pass
+    user_input = input(menu)
+
+
---
+++ database.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+
+CREATE_TABLE = """CREATE TABLE IF NOT EXISTS note (
+                category TEXT,
+                label TEXT,
+                content TEXT
+);"""
+
+INSERT_NOTE = "INSERT INTO note VALUES(?,?,?);"
+
+DISPLAY_NOTE = "SELECT * FROM note;" 
+
+SEARCH_CATEGORY = "SELECT * FROM note WHERE category = ?;"
+
+connection = sqlite3.connect("data.db")
+
+def create_table():
+    with connection:
+        connection.execute(CREATE_TABLE)
+
+def note(cat, lab, content):
+    with connection:
+        connection.execute(INSERT_NOTE, (cat, lab, content))
+
+def display():
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(DISPLAY_NOTE)
+        return cursor.fetchall()
+
+def search_cat(cat):
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(SEARCH_CATEGORY,(cat,))
+        return cursor.fetchall()

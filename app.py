641d99516b0d2abf99e1d0246b96ea30ae4fd3c2
Franz Tseng <blume918@gmail.com>
2021-04-30 21:37:10 -0400

    build search by keyword functionality and add timestamp to the table

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import database
-import datetime
 
 menu = """What would you like to do?
         1) Enter a new note
         2) Display all note
         3) Search notes by category
-        4) Search note by labels
-        5) Exit
+        4) Search notes by labels
+        5) Search keywords
+        6) Exit
 Your choice: """
 
 database.create_table()
@@ -16,49 +16,58 @@ def prompt_new_note():
     cat = input("Note category: ")
     lab = input("Note label: ")
     content = input("Enter your note: ")
-    database.note(content)
-    database.category(cat)
-    database.label(lab)
+    database.note(cat, lab, content)
 
 def show_note():
     notes = database.display()
     for note in notes:
-        print(f"""{note[0]} | {note[1]} 
+        print(f"""{note[2]} | {note[3]} on {note[1]} 
 -------------------------------------------  
-{note[2]}
+{note[4]}
 -------------------------------------------\n\n""")
 
-def show_search_result():
+def show_cat_search():
     cat = input("Enter the category your are looking for: ")
     search_result = database.search_cat(cat) 
     for search in search_result:
-        print(f"""\n{search[0]} | {search[1]} 
+        print(f"""\n{search[1]} | {search[2]} | {search[3]} 
 -------------------------------------------  
-{search[2]}
+{search[4]}
 -------------------------------------------\n\n""")
 
 def show_label_search():
     lab = input("Enter the label you are looking for: ")
     search_result = database.search_lab(lab)
     for search in search_result:
-        print(f"""\n{search[0]} | {search[1]} 
+        print(f"""\n{search[1]} | {search[2]} | {search[3]} 
 -------------------------------------------  
-{search[2]}
+{search[4]}
 -------------------------------------------\n\n""")
             
+def show_keyword_search():
+    keyword = input("Enter a keyword: ")
+    search_result = database.search_keyword(keyword)
+    for search in search_result:
+        print(f"""\n{search[1]} | {search[2]} | {search[3]} 
+-------------------------------------------  
+{search[4]}
+-------------------------------------------\n\n""")
 
     
 
 
-while user_input !="5":
+while user_input !="6":
     if user_input =="1":
         prompt_new_note()
     elif user_input =="2":
         show_note()
     elif user_input == "3":
-        show_search_result()
+        show_cat_search()
     elif user_input == "4":
         show_label_search()        
+    elif user_input == "5":
+        show_keyword_search()        
+
     user_input = input(menu)
 
 
--- database.py
+++ database.py
@@ -1,55 +1,38 @@
 import sqlite3
+import datetime
 
-
-CREATE_NOTE_TABLE = """CREATE TABLE IF NOT EXISTS note (
+CREATE_TABLE = """CREATE TABLE IF NOT EXISTS note (
                 id INTEGER PRIMARY KEY,
-                time REAL
+                time REAL,
+                category TEXT,
+                label TEXT,
                 content TEXT
 );"""
 
-CREATE_CATEGORY_TABLE = """CREATE TABLE IF NOT EXISTS categories (
-                category TEXT
-);"""
 
-CREATE_LABEL_TABLE = """CREATE TABLE IF NOT EXISTS labels (
-                        label TEXT,
-);"""
+INSERT_NOTE = """INSERT INTO note (time, category, label, content) 
+                VALUES(?,?, ?,?);"""
 
-INSERT_NOTE = "INSERT INTO note (time, content) VALUES(?,?);"
-
-INSERT_CATEGORY = "INSERT INTO categories VALUES(?);"
-
-INSERT_LABEL = "INSERT INTO labels VALUES(?);"
-
-DISPLAY_NOTE = """SELECT * FROM category
-JOIN note ON ;
-
-""" 
+DISPLAY_NOTE = "SELECT * FROM note"
 
 SEARCH_CATEGORY = "SELECT * FROM note WHERE category = ?;"
 
 SEARCH_LABEL = "SELECT * FROM note WHERE label = ?;"
 
+SEARCH_KEYWORD = "SELECT * FROM note WHERE content LIKE ?;"
+
 connection = sqlite3.connect("data.db")
 
 def create_table():
     with connection:
-        connection.execute(CREATE_NOTE_TABLE)
-        connection.execute(CREATE_CATEGORY_TABLE)
-        connection.execute(CREATE_LABEL_TABLE)
+        connection.execute(CREATE_TABLE)
     
-def note(content):
+def note(cat, lab, content):
     with connection:
         today_timestamp = datetime.datetime.today().timestamp()
-        connection.execute(INSERT_NOTE, (today_timestamp, content))
-
-def category(category):
-    with connection:
-        connection.execute(INSERT_CATEGORY,(category,)) 
-
-def label(lab):
-    with connection:
-        connection.execute(INSERT_LABELE, (lab,))
+        today = datetime.datetime.fromtimestamp(today_timestamp)
+        human_date = today.strftime("%Y/%m/%d")
+        connection.execute(INSERT_NOTE, (human_date, cat, lab, content))
 
 def display():
     with connection:
@@ -68,3 +51,10 @@ def search_lab(lab):
         cursor = connection.cursor()
         cursor.execute(SEARCH_LABEL, (lab,))
         return cursor.fetchall()
+
+def search_keyword(keyword):
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(SEARCH_KEYWORD, (f"% {keyword} %",))
+        return cursor.fetchall()
+
